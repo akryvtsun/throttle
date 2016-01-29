f9bf32fabab72054572584abc5f4277851e6c1fa
englishman <kontiky@gmail.com>
2016-01-29 07:31:15 -0500

    unit test creation

---
+++ src/test/java/org/throttle/ThrottleTest.java
@@ -0,0 +1,13 @@
+package org.throttle;
+
+import org.junit.Test;
+
+/**
+ * Created by englishman on 1/28/16.
+ */
+public class ThrottleTest {
+
+    @Test
+    public void testName() throws Exception {
+    }
+}
