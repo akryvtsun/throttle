6ba247394f9d4f3b70171aa36331e9547c7e3288
englishman <kontiky@gmail.com>
2016-02-01 23:18:29 -0500

    initial BurstThrottleStrategy impl

--- src/main/java/org/throttle/strategy/BurstThrottleStrategy.java
+++ src/main/java/org/throttle/strategy/BurstThrottleStrategy.java
@@ -9,20 +9,43 @@ import org.throttle.TimeService;
  */
 public class BurstThrottleStrategy implements ThrottleStrategy {
 
-    private final double rate;
-    private final int threshold;
+    private final double threshold;
+    private final int sizeThreshold;
     private final ThrottleInformer informer;
     private final TimeService timer;
 
-    public BurstThrottleStrategy(double rate, TimeService timer, int threshold, ThrottleInformer informer) {
-        this.rate = rate;
+    private boolean firstUsage = true;
+    private long lastTime;
+
+    public BurstThrottleStrategy(double rate, TimeService timer, int sizeThreshold, ThrottleInformer informer) {
+        this.threshold = 1000 / rate;
         this.timer = timer;
-        this.threshold = threshold;
+        this.sizeThreshold = sizeThreshold;
         this.informer = informer;
     }
 
     @Override
     public void acquire() throws InterruptedException {
-        // TODO implement logic
+        long currentTime = timer.getTime();
+
+        if (firstUsage) {
+            firstUsage = false;
+        }
+        else {
+            long delay = currentTime - lastTime;
+            if (delay < threshold) {
+                long effectiveDelay = getEffectiveDelay(delay);
+
+                timer.delay(effectiveDelay);
+            }
+        }
+
+        lastTime = currentTime;
+    }
+
+    private long getEffectiveDelay(long delay) {
+        return informer.getQueueSize() > sizeThreshold
+                ? 1     // execute ASAP
+                : (long) threshold - delay;
     }
 }
