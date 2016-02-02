eb3f6df39a8032f1c0b8fbdfeebbcc77826d4909
englishman <kontiky@gmail.com>
2016-02-02 01:09:28 -0500

    unify tests

--- src/test/java/org/throttle/strategy/RegularThrottleStrategyTest.java
+++ src/test/java/org/throttle/strategy/RegularThrottleStrategyTest.java
@@ -19,6 +19,7 @@ public class RegularThrottleStrategyTest {
     static final long T = 0L;                       // in millis
     static final double RATE = 4;                   // in TPS
     static final double THRESHOLD = 1000 / RATE;    // in millis
+    static final int SHIFT = 10;                    // in millis
 
     @Mock
     TimeService time;
@@ -39,17 +40,17 @@ public class RegularThrottleStrategyTest {
     @Test
     public void not_first_request_under_threshold_should_delay() throws Exception {
         doFirstRequest();
-        when(time.getTime()).thenReturn((long)(T + (THRESHOLD - 10)));
+        when(time.getTime()).thenReturn((long)(T + (THRESHOLD - SHIFT)));
 
         strategy.acquire();
 
-        verify(time, times(1)).delay(10);
+        verify(time, times(1)).delay(SHIFT);
     }
 
     @Test
     public void not_first_request_above_threshold_should_executed_without_delay() throws Exception {
         doFirstRequest();
-        when(time.getTime()).thenReturn((long)(T + (THRESHOLD + 1)));
+        when(time.getTime()).thenReturn((long)(T + (THRESHOLD + SHIFT)));
 
         strategy.acquire();
 
