6df11d33e5496692883719a10659a43d1a87a98a
englishman <kontiky@gmail.com>
2016-02-01 23:50:59 -0500

    create BurstThrottleStrategyTest

---
+++ src/test/java/org/throttle/strategy/BurstThrottleStrategyTest.java
@@ -0,0 +1,13 @@
+package org.throttle.strategy;
+
+import org.junit.Test;
+
+/**
+ * Created by englishman on 2/1/16.
+ */
+public class BurstThrottleStrategyTest {
+    @Test
+    public void testName() throws Exception {
+
+    }
+}
