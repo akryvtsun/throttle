e2380bd6f1ae2bef01e78405708de7f31f813de4
englishman <kontiky@gmail.com>
2016-02-04 11:15:51 -0500

    refactoring test

--- src/test/java/org/throttle/strategy/RegularThrottleStrategyTest.java
+++ src/test/java/org/throttle/strategy/RegularThrottleStrategyTest.java
@@ -33,45 +33,47 @@ public class RegularThrottleStrategyTest {
 
     @Test
     public void first_request_should_executed_without_delay() throws Exception {
-        doFirstRequest();
-        verify(time, never()).delay(anyLong());
+        askRequestInTime(T);
+        verifyNoTimeDelays();
     }
 
     @Test
     public void not_first_request_under_threshold_should_delay() throws Exception {
-        doFirstRequest();
-        when(time.getTime()).thenReturn((long)(T + (THRESHOLD - SHIFT)));
+        askRequestInTime(T);
 
-        strategy.acquire();
-
-        verify(time, times(1)).delay(SHIFT);
+        askRequestInTime((long)(T + (THRESHOLD - SHIFT)));
+        verifyTimeDelay(SHIFT);
     }
 
     @Test
-    public void request_3_under_threshold_should_delay() throws Exception {
-        doFirstRequest();
+    public void verify_3_sequential_requests_with_high_freq() throws Exception {
+        askRequestInTime(T);
 
-        when(time.getTime()).thenReturn((long)(T + (THRESHOLD - SHIFT)));
-        strategy.acquire();
-        verify(time, times(1)).delay(SHIFT);
+        askRequestInTime((long)(T + (THRESHOLD - SHIFT)));
+        verifyTimeDelay(SHIFT);
 
-        when(time.getTime()).thenReturn((long)(T + THRESHOLD + SHIFT));
-        strategy.acquire();
-        verify(time, times(1)).delay((long)(THRESHOLD - SHIFT));
+        askRequestInTime((long)(T + THRESHOLD + SHIFT));
+        verifyTimeDelay((long) (THRESHOLD - SHIFT));
     }
 
     @Test
     public void not_first_request_above_threshold_should_executed_without_delay() throws Exception {
-        doFirstRequest();
-        when(time.getTime()).thenReturn((long)(T + (THRESHOLD + SHIFT)));
+        askRequestInTime(T);
 
+        askRequestInTime((long)(T + (THRESHOLD + SHIFT)));
+        verifyNoTimeDelays();
+    }
+
+    private void askRequestInTime(long time) throws InterruptedException {
+        when(this.time.getTime()).thenReturn(time);
         strategy.acquire();
+    }
 
+    private void verifyNoTimeDelays() throws InterruptedException {
         verify(time, never()).delay(anyLong());
     }
 
-    private void doFirstRequest() throws InterruptedException {
-        when(time.getTime()).thenReturn(T);
-        strategy.acquire();
+    private void verifyTimeDelay(long duration) throws InterruptedException {
+        verify(time, times(1)).delay(duration);
     }
 }
